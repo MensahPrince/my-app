1ba069ed8e9dc7222999ef0445523b5d37783f38
MensahPrince <burnszephyr520@gmail.com>
2025-02-06 19:47:14 +0000

    Remove unused Roboto font import and simplify layout structure

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,14 @@
-import { Roboto } from "next/font/google"
+
 import "../styles/global.css"
 
-const roboto = Roboto({
-    weight: '400',
-    subsets: ["latin"],
-})
+
 
 
 export default function RootLayout({ children }) {
     return(
-        <html lang="en"  className={roboto.className}>
+        <html lang="en" >
             <body>
-                <header className="container"><h1>This is a header </h1></header>
                 <main>{children}</main>
-                <footer className="container">This is a Footer</footer>
             </body>
             
         </html>
